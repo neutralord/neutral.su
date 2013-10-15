acf2c85cd58d6466be82ec129c74b4a6d3bf0b02
Nikita Dementev <neutral@neutral.su>
2013-10-16 03:29:55 +0400

    Fixed code format

--- src/Neutral/PageBundle/Controller/PageController.php
+++ src/Neutral/PageBundle/Controller/PageController.php
@@ -4,6 +4,7 @@ namespace Neutral\PageBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Neutral\PageBundle\Entity\Page;
+
 class PageController extends Controller
 {
     public function viewAction(Page $page)
