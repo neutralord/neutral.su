df102ce9d38f4b9844a9d035bfdbbbafcf8315be
Nikita Dementev <neutral@neutral.su>
2013-11-28 14:32:09 +0400

    Hide unpublished posts from blog.

--- src/Neutral/BlogBundle/Controller/PostController.php
+++ src/Neutral/BlogBundle/Controller/PostController.php
@@ -4,18 +4,31 @@ namespace Neutral\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Neutral\BlogBundle\Entity\Post;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class PostController extends Controller
 {
     public function listAction()
     {
-        $em = $this->getDoctrine()->getManager();
-        $posts = $em->getRepository('NeutralBlogBundle:Post')->findAll();
+        $em = $this->getDoctrine()->getManager();$qb = $em->createQueryBuilder();
+        $query = $qb->add('select', 'p')
+            ->add('from', 'NeutralBlogBundle:Post p')
+        ;
+        if (!$this->get('security.context')->isGranted('ROLE_ADMIN')) {
+            $query->add('where', 'p.published = true');
+        }
+        $posts = $query->add('orderBy', 'p.createdAt DESC')
+            ->getQuery()
+            ->getResult()
+        ;
         return $this->render('NeutralBlogBundle:Post:list.html.twig', array('posts' => $posts));
     }
     
     public function postAction(Post $post)
     {
+        if (!$post->getPublished() && !$this->get('security.context')->isGranted('ROLE_ADMIN')) {
+            throw new NotFoundHttpException('Post is in the drafts.');
+        }
         return $this->render('NeutralBlogBundle:Post:post.html.twig', array('post' => $post));
     }
 }
